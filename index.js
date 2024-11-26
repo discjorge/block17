652501fb8eae9d685a93f117ab050ea0260fd82f
Jorge Barrios <jorge@LAPTOP-GI60HTFM.>
2024-11-25 20:25:54 -0600

    final files

--- index.js
+++ index.js
@@ -11,27 +11,70 @@ class Numbers {
   }
   count() {
     //return the count of numbers in data
+    return this.data.length;
+    //return this.data.split(",").length;
   }
   printNumbers() {
     //print the numbers in data
+    console.log(this.data);
   }
   odds() {
     //return the odd numbers in data
+    //return the odd numbers in data
+    const oddArray = [];
+    this.data.forEach((num) => {
+      if (num % 2 !== 0) {
+        oddArray.push(num);
+      }
+    });
+
+    return oddArray;
   }
   evens() {
     //return the even numbers in data
+    const oddArray = [];
+    this.data.forEach((num) => {
+      if (num % 2 === 0) {
+        oddArray.push(num);
+      }
+    });
+
+    return oddArray;
   }
   sum() {
     //return the sum of the numbers
+    let sum = 0;
+    for (let i = 0; i < this.data.length; i++) {
+      sum += this.data[i];
+    }
+    return sum;
   }
   product() {
     //return the product of the numbers
+    let product = 1;
+    for (let i = 0; i < this.data.length; i++) {
+      product *= this.data[i];
+    }
+    return product;
   }
   greaterThan(target) {
     //return the numbers greater than the target
+    const greaterThanArray = [];
+    this.data.forEach((num) => {
+      if (num > target) {
+        greaterThanArray.push(num);
+      }
+    });
+
+    return greaterThanArray;
   }
   howMany(target) {
     //return the count of a given number
+    let count = 0;
+    this.data.forEach((num) => {
+      if (num == target) count++;
+    });
+    return count;
   }
 }
 
